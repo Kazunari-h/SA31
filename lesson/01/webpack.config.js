06575f31c17e245002d03f598847795b7099dde2
Kazunari-h <kazunario456@gmail.com>
2020-11-13 11:56:58 +0900

    webpack.config.jsの追加

---
+++ lesson/01/webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // メインとなるJavaScriptファイル（エントリーポイント）
+  entry: ``,
+  // ファイルの出力設定
+  output: {
+    //  出力ファイルのディレクトリ名
+    path: ``,
+    // 出力ファイル名
+    filename: "bundle.js"
+  }
+};
